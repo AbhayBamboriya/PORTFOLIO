7d4e93473b8ed3492c79459b06a66d5e46fbe865
AbhayBamboriya <bamboriya09@gmail.com>
2024-06-11 17:57:46 +0530

    Functionality of framer motion

--- PORTFOLIO 2/src/Test.jsx
+++ PORTFOLIO 2/src/Test.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import './app.scss'
 function Test() {
+    const [open,setOpen]=useState(false)
+    const variants={
+        visible:{opacity:1,x:500},
+        hidden:{opacity:0}
+    }
   return (
     <div className='course'>
       <motion.div
-        initial={{opacity:0,scale:0.5}}
-       animate={{opacity:1,scale:1}}
-       transition={{duration:2}}
-       whileHover={{opacity:1,scale:2}}
-       drag
+        // initial={{opacity:0,scale:0.5}}
+        variants={variants}
+        transition={{type:"spring",stiffness:100}}
+        // initial="hidden"
+        // animate="visible"
+        animate={open?"visible":"hidden"}
         className="box"></motion.div>
+        <button onClick={()=>setOpen(pre=>!pre)}>Click</button>
     </div>
   )
 }
